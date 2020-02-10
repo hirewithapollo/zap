2decbf7ea0347496c06ac4aed054b5eeb2ace50c
Stephen Meriwether <stephen@meriwether.io>
2020-02-10 07:39:15 -0500

    send email data to zapier

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const createError = require('http-errors');
 const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+require('dotenv').config()
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const router = express.Router();
-router.post('/', function(req, res, next) {
-  const email = req.body.email;
-  const timestamp = new Date();
-  console.log(email);
-  console.log(timestamp);
-  return res.json('done');
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://hirewithapollo.com' : '*',
+  optionsSuccessStatus: 200,
+};
+
+app.options('/', cors(corsOptions))
+app.post('/', cors(corsOptions), async (req, res) => {
+  try {
+    await fetch(process.env.ZAP_URL, {
+      method: 'post',
+      body:    JSON.stringify({ email: req.body.email, timestamp: new Date() }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    return res.json('done');
+  } catch {
+    return res.json('error').status(500);
+  }
 });
 
-app.use('/', router);
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
